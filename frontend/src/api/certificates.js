7896dd474657245239a66ba25b36a31bb97861e6
fedeit <fede.galbi@hotmail.com>
2020-10-06 02:09:35 +0200

    Implemented a JSON file with certificates

--- frontend/src/api/certificates.js
+++ frontend/src/api/certificates.js
@@ -8,18 +8,10 @@
 
 */
 
-// List of certificates to be managed by the system
-let certificates = {
-	"wYgaaTEsQO": {
-		type: "Volunteering Certificate",
-		recipient: "First Name",
-		date: "1 January 2020",
-		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras finibus urna at risus tempor faucibus. Praesent nibh erat, gravida id tristique ac, pellentesque sit amet arcu. Quisque nisi nunc, aliquet quis lorem a, blandit fringilla dolor. Ut venenatis metus laoreet porta hendrerit. Aenean a fringilla arcu. Aenean consectetur leo eget lectus molestie, eu dignissim arcu tempus. Maecenas sit amet interdum libero. Mauris semper vehicula tincidunt. Nunc egestas ut enim eget cursus",
-		signature: "Signature",
-		sponsor: "assets/img/sponsor.png"
-	}
-}
-
+// Get certificates from JSON file
+import * as parsedJSON from '../data/certs.json';
+//Get an Object of certs from the JSON content
+const certificates = parsedJSON.default;
 // Functiont to get the certificate by id and return it
 let getCertificate = (id) => {
 	return certificates[id];
