317af0425fb6d81f77ea06046557793d08f32518
fedeit <fede.galbi@hotmail.com>
2020-10-06 02:03:41 +0200

    Removed useless vars

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // Import components
@@ -7,19 +6,7 @@ import Navbar from './Navbar';
 import Certificate from './Certificate';
 import Footer from './Footer';
 
-let absoluteStyle = {
-    position: 'absolute', width: '100%'
-}
-
 function App() {
-    let certificate = {
-        type: "Volunteering Certificate",
-        recipient: "First Name",
-        date: "1 January 2020",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras finibus urna at risus tempor faucibus. Praesent nibh erat, gravida id tristique ac, pellentesque sit amet arcu. Quisque nisi nunc, aliquet quis lorem a, blandit fringilla dolor. Ut venenatis metus laoreet porta hendrerit. Aenean a fringilla arcu. Aenean consectetur leo eget lectus molestie, eu dignissim arcu tempus. Maecenas sit amet interdum libero. Mauris semper vehicula tincidunt. Nunc egestas ut enim eget cursus",
-        signature: "Signature"
-    }
-
   return (
     <div className="App">
       <Navbar />
